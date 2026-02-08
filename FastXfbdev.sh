2ff3e6c78bc2efb554e3f5740b2c655d0323e260
inecraftm84 <158472314+inecraftm84@users.noreply.github.com>
2026-02-08 19:26:00 +0800

    Rename Xfbdev.sh to FastXfbdev.sh

--- FastXfbdev.sh
+++ FastXfbdev.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
+# quit Using ctrl+alt+backspace
 pmn install Xfbdev xterm twm xfonts-base xfonts-utils
 cd /usr/share/fonts/X11/misc
 mkfontdir
 echo "fixed -misc-fixed-medium-r-semicondensed--13-120-75-75-c-60-iso8859-1" > fonts.alias
 /usr/local/bin/Xfbdev -fp /usr/share/fonts/X11/misc -screen 1024x768x16
+twm &
+xterm &
