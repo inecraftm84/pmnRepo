bb5af4927a498c12954143f39473f492e20e6b3d
inecraftm84 <158472314+inecraftm84@users.noreply.github.com>
2026-02-08 19:28:21 +0800

    Update FastXfbdev.sh

--- FastXfbdev.sh
+++ FastXfbdev.sh
@@ -5,5 +5,7 @@ cd /usr/share/fonts/X11/misc
 mkfontdir
 echo "fixed -misc-fixed-medium-r-semicondensed--13-120-75-75-c-60-iso8859-1" > fonts.alias
 /usr/local/bin/Xfbdev -fp /usr/share/fonts/X11/misc -screen 1024x768x16 &
+sleep 2
+export DISPLAY=:0
 twm &
 xterm &
