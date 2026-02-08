17a12635afcd99c12746d27851d1ec039afc7f6c
inecraftm84 <158472314+inecraftm84@users.noreply.github.com>
2026-02-08 19:26:47 +0800

    Update FastXfbdev.sh

--- FastXfbdev.sh
+++ FastXfbdev.sh
@@ -4,6 +4,6 @@ pmn install Xfbdev xterm twm xfonts-base xfonts-utils
 cd /usr/share/fonts/X11/misc
 mkfontdir
 echo "fixed -misc-fixed-medium-r-semicondensed--13-120-75-75-c-60-iso8859-1" > fonts.alias
-/usr/local/bin/Xfbdev -fp /usr/share/fonts/X11/misc -screen 1024x768x16
+/usr/local/bin/Xfbdev -fp /usr/share/fonts/X11/misc -screen 1024x768x16 &
 twm &
 xterm &
