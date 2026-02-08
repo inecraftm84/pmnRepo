b8d470ecacc2c71abe14bcebcad271b808540b36
inecraftm84 <158472314+inecraftm84@users.noreply.github.com>
2026-02-08 19:21:24 +0800

    Add Xfbdev installation script

---
+++ Xfbdev.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+pmn install Xfbdev xterm twm xfonts-base xfonts-utils
+cd /usr/share/fonts/X11/misc
+mkfontdir
+echo "fixed -misc-fixed-medium-r-semicondensed--13-120-75-75-c-60-iso8859-1" > fonts.alias
+/usr/local/bin/Xfbdev -fp /usr/share/fonts/X11/misc -screen 1024x768x16
